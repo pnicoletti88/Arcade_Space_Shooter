498ed61817d282de2202e5b10573a0b29dbefefe
Garvan Doyle <garvandoyle@gmail.com>
2019-02-20 20:27:27 -0500

    Garvan Part 2, Enemy 0 needs bounds checking

---
+++ Assets/Enemy_0.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Enemy_0 : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Move();
+        
+    }
+    public Vector3 position
+    {
+        get
+        {
+            return (this.transform.position);
+        }
+        set
+        {
+            this.transform.position = value;
+        }
+    }
+
+    public void Move()
+    {
+        Vector3 temporaryPosition = position;
+        temporaryPosition.y -= 10f * Time.deltaTime;
+        position = temporaryPosition;
+    }
+}
